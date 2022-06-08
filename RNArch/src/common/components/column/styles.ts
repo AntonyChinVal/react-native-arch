d9e2b293e0142c1eb1a15601020677aedd7f0b62
Antony David Chinchay Valdivia <antonychinchay@bcp.com.pe>
2022-06-08 07:11:07 -0500

    Components added

---
+++ RNArch/src/common/components/column/styles.ts
@@ -0,0 +1,9 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'column',
+    flex: 1,
+  },
+});
+export default styles;
---
+++ RNArch/src/common/components/row/styles.ts
@@ -0,0 +1,9 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    flex: 1,
+  },
+});
+export default styles;
