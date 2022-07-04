6132efc67191d2b98e6681e96de46c8aa05ffbaa
Antony David Chinchay Valdivia <antonychinchay@bcp.com.pe>
2022-07-04 13:54:22 -0500

    Refactor

--- RNArch/src/api/NetworkService.ts
+++
@@ -1,7 +0,0 @@
-import { AxiosRequestConfig, AxiosResponse } from 'axios';
-
-export const NETWORK_SERVICE_TYPE = Symbol();
-
-export interface NetworkService {
-  request<T>(config: AxiosRequestConfig): Promise<AxiosResponse<T>>;
-}
