2b212be3dbf065e1e1f5aa23c22981fdd5153ee4
Antony David Chinchay Valdivia <antonychinchay@bcp.com.pe>
2022-07-04 13:45:35 -0500

    Husky Test

--- RNArch/src/services/authService/index.ts
+++ RNArch/src/services/authService/index.ts
@@ -1,11 +1,13 @@
 import { networkService } from 'api';
+import { NetworkService } from 'api/networkService';
 import { injectable } from 'inversify';
 import { AuthService, SigningParams } from 'services/authService/authService';
 
 @injectable()
 export class AuthServiceImpl implements AuthService {
   async signing(params: SigningParams): Promise<string> {
-    const response = await networkService.request<string>({
+    const networkServiceInner: NetworkService = networkService;
+    const response = await networkServiceInner.request<string>({
       method: 'POST',
       url: '/auth/signin',
       data: params,
