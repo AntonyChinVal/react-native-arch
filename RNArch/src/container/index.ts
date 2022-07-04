c60d9edb37fc77665495c828f9b9a70a1f04b751
Antony David Chinchay Valdivia <antonychinchay@bcp.com.pe>
2022-07-04 13:44:22 -0500

    Husky Test

--- RNArch/src/container/index.ts
+++ RNArch/src/container/index.ts
@@ -1,3 +1,5 @@
+import { NetworkServiceImpl } from 'api';
+import { NetworkService } from 'api/networkService';
 import { Container } from 'inversify';
 import { AuthServiceImpl } from 'services/authService';
 import { AuthService, AUTH_SERVICE_TYPE } from 'services/authService/authService';
@@ -6,6 +8,7 @@ import { UserService, USER_SERVICE_TYPE } from 'services/userService/userService
 
 const container = new Container();
 
+container.bind<NetworkService>(AUTH_SERVICE_TYPE).to(NetworkServiceImpl);
 container.bind<AuthService>(AUTH_SERVICE_TYPE).to(AuthServiceImpl);
 container.bind<UserService>(USER_SERVICE_TYPE).to(UserServiceImpl);
 
