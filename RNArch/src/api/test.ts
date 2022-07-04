98d228171e0d6a00d15a0113c386f0ab063ac099
Antony David Chinchay Valdivia <antonychinchay@bcp.com.pe>
2022-07-04 13:49:09 -0500

    Husky Test

--- RNArch/src/api/test.ts
+++ RNArch/src/api/test.ts
@@ -3,7 +3,7 @@ import { Test2 } from './test2';
 export class Test {
   public test2(): void {
     const test2 = new Test2();
+    test2.test2();
     console.log(test2);
-    // ...
   }
 }
--- RNArch/src/api/test2.ts
+++ RNArch/src/api/test2.ts
@@ -3,6 +3,7 @@ import { Test } from './test';
 export class Test2 {
   public test2(): void {
     const test2 = new Test();
+    test2.test2();
     console.log(test2);
   }
 }
